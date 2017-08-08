85bbf2d9011f5b999cab55f649ed8cf4646a586c
Wallison Freitas <wallisonfreitas00@gmail.com>
2017-08-08 20:36:11 -0300

    criando e atribuindo variáveis a classe negociação

---
+++ client/js/app/models/Negociacao.js
@@ -0,0 +1,10 @@
+class Negociacao {
+
+  //a função constructor() vai definir os atributos de uma classe
+  constructor(){
+
+      this.data = new Date();
+      this.quantidade = 1;
+      this.valor = 0.0;
+  }
+}
